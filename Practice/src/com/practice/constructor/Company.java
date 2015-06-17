015b3c27c106f1cf1cc34e822ff6227cd62a40c7
vandanamodey <rajatmodey@192.168.1.108>
2015-06-17 17:45:28 -0400

    Assignment-Constr.Overloading

---
+++ Practice/src/com/practice/constructor/Company.java
@@ -0,0 +1,23 @@
+package com.practice.constructor;
+
+// Assignment to use Constructor with different arguements
+
+public class Company {
+
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee(001, "Judy");
+		System.out.println("Employee #1  ID : " + emp1.getId() + " Name : "
+				+ emp1.getName());
+
+		Employee emp2 = new Employee(002, "Joanna");
+		System.out.println("Employee #2  ID : " + emp2.getId() + " Name : "
+				+ emp2.getName());
+
+		Employee emp3 = new Employee(003, "Jackson", "ACCOUNTS");
+		System.out.println("Employee #3  ID : " + emp3.getId() + " Dept : "
+				+ emp3.getDepartment());
+
+	}
+
+}
--- Practice/src/com/practice/constructor/Employee.java
+++ Practice/src/com/practice/constructor/Employee.java
@@ -4,44 +4,52 @@ public class Employee {
 	private String name;
 	private int id;
 	private String department;
-	
-	
-	Employee(){
-		
+
+	Employee() {
+
 	}
-	
+
 	// 1 argument
-	public Employee (String nameLocal){
+	public Employee(String nameLocal) {
 		this.name = nameLocal;
-		
+
 	}
-	
-	//2 arguments
-	public Employee (int id, String department){
+
+	public Employee(int id, String nameLocal) {
+		this.id = id;
+		this.name = nameLocal;
+
+	}
+
+	// 2 arguments
+	public Employee(int id, String nameLocal, String department) {
 		this.id = id;
 		this.department = department;
+		this.name = nameLocal;
 	}
-	
-	
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	
-	
+
 }
